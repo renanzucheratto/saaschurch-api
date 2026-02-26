ea07ce85257242f2e333cb1ca3bc144e48757eac
renanzucheratto <renan.loboz@gmail.com>
2026-02-26 19:31:55 -0300

    feat: package e server ts

--- src/server.ts
+++ src/server.ts
@@ -20,6 +20,6 @@ app.use(express.json());
 
 app.use('/eventos', eventosRoutes);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on port ' + (process.env.PORT ?? 3000));
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
