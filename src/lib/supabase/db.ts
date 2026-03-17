4b4578331d65f9e414cc275a20f3ec7e34aafd48
renanzucheratto <renan.loboz@gmail.com>
2026-03-16 22:43:50 -0300

    feat: variavel de ambiente db.ts

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = "https://cahyjgtrverpbtjaobdg.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhaHlqZ3RydmVycGJ0amFvYmRnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2Nzk4ODQ5MCwiZXhwIjoyMDgzNTY0NDkwfQ.TdzdUKIs8nYHcyWpaF_HsGDk_54vijHLleMiurcXwOw";
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 export const sb = createClient(supabaseUrl, supabaseKey);
